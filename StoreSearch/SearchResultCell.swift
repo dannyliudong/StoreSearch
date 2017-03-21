b2fae702acea5f1749742407627df02a15e264ad
liudong <sunmono@126.com>
2017-03-21 13:51:22 +0800

    添加app 图标，设置搜索栏样式

--- StoreSearch/SearchResultCell.swift
+++ StoreSearch/SearchResultCell.swift
@@ -14,9 +14,19 @@ class SearchResultCell: UITableViewCell {
     @IBOutlet weak var artistNameLabel: UILabel!
     @IBOutlet weak var artworkImageView: UIImageView!
     
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        print("init time: \(NSDate())")
+    }
+    
     override func awakeFromNib() {
+        print("awakeFromNib time: \(NSDate())")
         super.awakeFromNib()
         // Initialization code
+        
+        let selectedView = UIView(frame: CGRect.zero)
+        selectedView.backgroundColor = UIColor(red: 20/255, green: 160/255, blue: 160/255, alpha: 1)
+        selectedBackgroundView = selectedView
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
